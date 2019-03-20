7d99e2a15612d83872d3ea4805296ac7935e0b48
Joseph Abell <joseph.abell@piksel.com>
2019-03-20 20:51:32 +0000

    slides change

---
+++ code/021.1 - running postgraphile with jwt set up.sh
@@ -0,0 +1,1 @@
+npx postgraphile --connection postgres://forum_postgraphile:s883ZabA9D8jVBXd@139.162.212.109/forum --schema forum --default-role forum_anonymous --token forum.json_web_token --secret 4k8ZmdPLxWK71j8F --enhance-graphiql --graphql / --cors
